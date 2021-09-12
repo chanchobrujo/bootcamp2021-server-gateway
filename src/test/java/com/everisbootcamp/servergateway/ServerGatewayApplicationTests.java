d22a393c56a3a521f03463daf8de409928deb824
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 15:42:42 -0500

    Update 1

---
+++ src/test/java/com/everisbootcamp/servergateway/ServerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.servergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
