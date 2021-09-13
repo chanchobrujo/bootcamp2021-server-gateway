68ea5164c0a1d0081a624710b366df68b8ed6ef9
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 21:54:04 -0500

    Update 2

--- src/main/java/com/everisbootcamp/servergateway/ServerGatewayApplication.java
+++ src/main/java/com/everisbootcamp/servergateway/ServerGatewayApplication.java
@@ -2,12 +2,18 @@ package com.everisbootcamp.servergateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableEurekaClient
 @SpringBootApplication
 public class ServerGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerGatewayApplication.class, args);
+		log.info("SERVER GATEWAY ENABLED"); 
 	}
 
 }
